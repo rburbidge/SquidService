94fa1c9fc03752d3d3a25aff0e7abcec443cb38b
Ryan Burbidge <Ryan Burbidge>
2016-04-27 21:56:33 -0700

    Removed more stuff - Removed all code from server.js except basic hello world response - Removed dependencies on jade and other packages - Removed views

--- server.js
+++ server.js
@@ -1,66 +1,7 @@
-
-/**
-* MODULE DEPENDENCIES
-* -------------------------------------------------------------------------------------------------
-* include any modules you will use through out the file
-**/
-
 var express = require('express')
-  , http = require('http')
-  , nconf = require('nconf')
-  , path = require('path')
-  , everyauth = require('everyauth')
-  , Recaptcha = require('recaptcha').Recaptcha;
-
-
-/**
-* CONFIGURATION
-* -------------------------------------------------------------------------------------------------
-* load configuration settings from ENV, then settings.json.  Contains keys for OAuth logins. See 
-* settings.example.json.  
-**/
-nconf.env().file({ file: 'settings.json' });
 
 var app = express();
-app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-
-    // TODO Use a different site icon
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('azure zomg'));
-    app.use(express.session());
-    app.use(everyauth.middleware(app));
-    app.use(app.router);
-    app.use(require('less-middleware')({ src: __dirname + '/public' }));
-    app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+	res.send('Hello world');
 });
-
-app.configure('development', function () {
-    app.use(express.errorHandler());
-});
-
-/**
-* ROUTING
-* -------------------------------------------------------------------------------------------------
-* include a route file for each major area of functionality in the site
-**/
-require('./routes/home')(app);
-require('./routes/account')(app);
-
-
-var server = http.createServer(app);
-
-/**
-* RUN
-* -------------------------------------------------------------------------------------------------
-* this starts up the server on the given port
-**/
-
-server.listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
-});
+app.listen(process.env.PORT || 3000);
