a001f8ee4c8d3f584b2fe1b7e285613f3ed4f586
Ryan Burbidge <Ryan Burbidge>
2016-04-27 22:24:09 -0700

    Minimal server with package.json for npm package installation, server, and Azure files (web.config and iisnode.yml).

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express')
 
 var app = express();
 app.get('/', function(req, res) {
-	res.send('Hello world');
+	res.send('Sir Nommington');
 });
 app.listen(process.env.PORT || 3000);
