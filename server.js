ee0fe1eb7c51e5514d3417d52d5b1c037215d85e
Ryan Burbidge <Ryan Burbidge>
2016-04-28 20:38:43 -0700

    Change to return 200

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 
 var app = express();
 app.get('/', function(req, res) {
-	res.send('Sir Nommington');
+    res.writeHead(200);
+    res.end('Sir Nommington');
 });
 app.listen(process.env.PORT || 3000);
