6c08c56f9c2acf52a7ffd92a82beac2f06bfa67f
rburbidge <rburbidge@rim.com>
2017-08-28 22:50:36 -0700

    Refactored use of promises (#5)
    Avoid creating promises and use promise chaining instead. Added return types to promises

--- data/devices.ts
+++ data/devices.ts
@@ -31,36 +31,28 @@ export class Devices {
         
         // Tries to add the device to an existing user in the database. If the user doesn't exist, then adds the
         // device under a new user
-        return new Promise((resolve, reject) => {
-            this.collection.updateOne({ userId: userId }, { $push: { devices: device }})
-                .then(result => {
-                    // Device added to existing user
-                    if(result.modifiedCount === 1) {
-                        console.log(`Device ${deviceId} added to existing user`);
-                        resolve(device);
-                        return;
-                    }
+        return this.collection.updateOne({ userId: userId }, { $push: { devices: device }})
+            .then((result: mongodb.UpdateWriteOpResult) => {
+                // Device added to existing user
+                if(result.modifiedCount === 1) {
+                    console.log(`Device ${deviceId} added to existing user`);
+                    return Promise.resolve(device);
+                }
 
-                    // New user with new device
-                    let user: User = {
-                        userId: userId,
-                        devices: [ device ]
-                    };
-                    this.collection.insert(user)
-                        .then(result => {
-                            if(result.insertedCount === 1) {
-                                console.log(`Device ${deviceId} added to new user`);
-                                resolve(device);
-                                return;
-                            }
-
-                            console.log('ERROR: Unable to add device to new user');
-                            reject();
-                        })
-                        .catch(reject);
-                })
-                .catch(reject);
-        });
+                // New user with new device
+                let user: User = {
+                    userId: userId,
+                    devices: [ device ]
+                };
+                return this.collection.insert(user)
+                    .then(result => {
+                        if(result.insertedCount === 1) {
+                            console.log(`Device ${deviceId} added to new user`);
+                            return device;
+                        }
+                        throw new Error('ERROR: Unable to add device to new user');
+                    })
+            });
     }
 
     /**
@@ -76,10 +68,11 @@ export class Devices {
      * @param userId The user ID.
      * @param deviceId The device ID.
      */
-    public removeDevice(userId: string, deviceId: string): Promise<any> {
+    public removeDevice(userId: string, deviceId: string): Promise<boolean> {
         let unsetObj = { $unset: {} };
         unsetObj.$unset['devices.' + deviceId] = 1;
 
-        return this.collection.updateOne({ userId: userId }, { $pull: { 'devices' : { id: deviceId }}});
+        return this.collection.updateOne({ userId: userId }, { $pull: { 'devices' : { id: deviceId }}})
+            .then(result => result.modifiedCount > 0);
     }
 }
