629917561c5ce13da2516389d1be364030e3cd52
rburbidge <ryan.burbidge@gmail.com>
2018-04-23 19:32:45 -0700

    Add test for app insights partial

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,26 @@
 import { testFixture } from './setup';
 
 import * as request from 'supertest';
+import * as assert from 'assert';
+import { assertContains } from './helpers';
 
 describe('@e2e index', () => {
-    it('GET base url should return 200', () =>
-        request(testFixture.server)
-            .get('')
-            .expect(200)
-            .expect('Hello Server')
-    );
+    describe('GET ""', () => {
+        it('Should return 200 and contains "Sir Nommington"', () => {
+            return request(testFixture.server)
+                .get('')
+                .expect(200)
+                .expect((res) => {
+                    assertContains(res.text, 'Sir Nommington');
+                });
+        });
+
+        it('Should return page with app insights', () => {
+            return request(testFixture.server)
+                .get('')
+                .expect((res) => {
+                    assertContains(res.text, 'var appInsights=window.appInsights');
+                });
+        });
+    });
 });
