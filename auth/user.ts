781ff8cf7c2130c6d0c97c4e9728c8698e0c2d67
rburbidge <ryan.burbidge@gmail.com>
2018-04-24 21:33:48 -0700

    Refactor ID prefix code

--- auth/user.ts
+++ auth/user.ts
@@ -2,15 +2,20 @@ import { IGoogleUserInfo, IGoogleIdToken } from '../services/google'
 
 /** An identity object that is created during authentication. */
 export class User {
+    public readonly id: string;
+
     /**
      * @param id Unique user ID.
      */
     private constructor(
-        public readonly id: string,
+        id: string,
         public readonly name: string,
         public readonly picture: string,
         public readonly email?: string,
-        public readonly gender?: string) { }
+        public readonly gender?: string)
+    {
+        this.id = User.GoogleIdPrefix + id;
+    }
 
     /** The prefix used on Google IDs */
     private static GoogleIdPrefix = "google-";
@@ -20,7 +25,7 @@ export class User {
      * The Google ID is scoped with "google-" prefix.
      */
     public static fromUserInfo(userInfo: IGoogleUserInfo): User {
-        return new User(User.GoogleIdPrefix + userInfo.sub, userInfo.name, userInfo.picture, userInfo.email, userInfo.gender);
+        return new User(userInfo.sub, userInfo.name, userInfo.picture, userInfo.email, userInfo.gender);
     }
 
     /**
@@ -28,6 +33,6 @@ export class User {
      * The Google ID is scoped with "google-" prefix.
      */
     public static fromIdToken(token: IGoogleIdToken): User {
-        return new User(User.GoogleIdPrefix + token.sub, token.name, token.picture);
+        return new User(token.sub, token.name, token.picture);
     }
 }
